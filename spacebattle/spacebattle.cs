899568c34404722b6ab20f23b457a6c9f5b99866
Maksim_Smolenkov <makssmolenkovmail.ru@gmail.com>
2023-07-03 08:48:35 +0000

    Edit spacebattle

--- spacebattle/spacebattle.cs
+++ spacebattle/spacebattle.cs
@@ -3,7 +3,7 @@ public class SpaceBattle
 {
     public static double[] StraightMove(double[] start, double[] speed)
     {
-        double[] empty = new double{};
+        double[] empty = new double[] {};
         if (start == empty || speed == empty || start[0] == double.NaN || start[1] == double.NaN || speed[0] == double.NaN || speed[1] == double.NaN || speed[0] == double.PositiveInfinity || speed[1] == double.PositiveInfinity || start[0] == double.PositiveInfinity || start[1] == double.PositiveInfinity || speed[0] == double.NegativeInfinity || speed[1] == double.NegativeInfinity || start[0] == double.NegativeInfinity || start[1] == double.NegativeInfinity)
         {
             throw new System.ArgumentException();
