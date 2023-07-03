b4bcfc679687c1af3ad43979afc1200c18426a8b
MaksimSmolenkov <125339609+MaksimSmolenkov@users.noreply.github.com>
2023-07-03 20:33:41 +0600

    Update spacebattle.cs

--- spacebattle/spacebattle.cs
+++ spacebattle/spacebattle.cs
@@ -5,11 +5,11 @@ public class SpaceBattle
     {
         double[] empty = new double[] {};
         double[] finish = new double[2];
-        if (start == empty || start[0] == double.NaN || start[1] == double.NaN || start[0] == double.PositiveInfinity || start[1] == double.PositiveInfinity || start[0] == double.NegativeInfinity || start[1] == double.NegativeInfinity)
+        if (start == empty || start[0] == Double.NaN || start[1] == Double.NaN || start[0] == Double.PositiveInfinity || start[1] == Double.PositiveInfinity || start[0] == Double.NegativeInfinity || start[1] == Double.NegativeInfinity)
         {
             throw new System.ArgumentException();
         }
-        else if (speed == empty || speed[0] == double.NaN || speed[1] == double.NaN || speed[0] == double.PositiveInfinity || speed[1] == double.PositiveInfinity || speed[0] == double.NegativeInfinity || speed[1] == double.NegativeInfinity)
+        else if (speed == empty || speed[0] == Double.NaN || speed[1] == Double.NaN || speed[0] == Double.PositiveInfinity || speed[1] == Double.PositiveInfinity || speed[0] == Double.NegativeInfinity || speed[1] == Double.NegativeInfinity)
         {
             throw new System.ArgumentException();
         }
